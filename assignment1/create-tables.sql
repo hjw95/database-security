837b958166bccf03db3522529e20ad0c5a14566a
Hady Willi <hadyjanuar95@gmail.com>
2018-10-05 11:55:21 +0800

    Add comments for db startup process, add create roles file separate from creating tables

--- assignment1/create-tables.sql
+++ assignment1/create-tables.sql
@@ -1,11 +1,13 @@
 -- Run dbstart $ORACLE_HOME
+-- sqlplus / as sysdba;
+-- startup;
 -- Save the file somewhere with some-name.sql
 -- Run “sqlplus”, log in
 -- Run “@some-name.sql”
 -- Place to write scripts
-
-
 -- Table Creation
+
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE; 
 
 DROP    TABLE School;
 CREATE  TABLE School (
@@ -64,42 +66,3 @@ CREATE  TABLE Loan (
     REFERENCES Bill(bill_id),
     CONSTRAINT loan_pk primary key(bill_id,loan_type)
 );
-
--- Creating Roles
-
-ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE; 
-
-DROP    ROLE Student;
-CREATE  ROLE Student             IDENTIFIED BY poorandindebt;
-
-DROP    ROLE Lecturer;
-CREATE  ROLE Lecturer            IDENTIFIED BY teachingforfun;
-
-DROP    ROLE StaffAdmin;
-CREATE  ROLE StaffAdmin          IDENTIFIED BY fearmelecturers;
-
-DROP    ROLE EnrollmentAdmin;
-CREATE  ROLE EnrollmentAdmin     IDENTIFIED BY iaddcoursesandstudents;
-
--- Granting Public Access
-
-GRANT SELECT ON School TO PUBLIC;
-
-GRANT SELECT ON Courses TO Public;
-
-GRANT SELECT ON Staff(staff_id, name, email, title, phone_number, office_location, school_id) TO PUBLIC;
-
--- Granting Student Access
-
-GRANT SELECT ON Grade TO Student;
-
-GRANT SELECT ON Bill TO Student;
-
-GRANT SELECT ON Student TO Student;
-
-CREATE OR REPLACE FUNCTION StudentGrade(v_schema IN VARCHAR2, v_obj IN VARCHAR2)
-RETURN VARCHAR2 AS condition VARCHAR2(200);
-BEGIN
-    condition := " ";
-    RETURN condition;
-END StudentGrade;

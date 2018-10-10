dd9b28d3287bed5002d5a050077159cda3db3497
Hady Willi <hadyjanuar95@gmail.com>
2018-10-10 20:56:40 +0800

    Fix create table typo and create roles for staff

--- assignment1/1.create-tables.sql
+++ assignment1/1.create-tables.sql
@@ -61,7 +61,7 @@ CREATE  TABLE Student (
 
 CREATE  TABLE Course (
     course_id               varchar(10)     NOT NULL PRIMARY KEY,
-    course_name             varchar(100)    NOT NULL
+    course_name             varchar(100)    NOT NULL,
     location                varchar(100)    NOT NULL,
     class_time              varchar(100)    NOT NULL,
     semester                int             NOT NULL,
@@ -78,7 +78,7 @@ CREATE  TABLE Enrollment (
     course_id               varchar(10)     NOT NULL
                             CONSTRAINT fkey_enrollment_course REFERENCES Course(course_id),
     grade                   NUMBER(6,2)     NOT NULL,
-                            CONSTRAINT pkey_enrollment PRIMARY KEY (student_id, course_id)
+    CONSTRAINT pkey_enrollment PRIMARY KEY (student_id, course_id)
 );
 
 CREATE  TABLE Bill (

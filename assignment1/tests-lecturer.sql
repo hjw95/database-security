5a1843014c60b03645aefda0f842fd8fa3a9e849
Hady Willi <hadyjanuar95@gmail.com>
2018-10-21 15:26:58 +0800

    Update lecturer test

--- assignment1/tests-lecturer.sql
+++ assignment1/tests-lecturer.sql
@@ -8,7 +8,11 @@ SELECT * FROM dev.Course;
 SELECT * FROM dev.Enrollment;
 SELECT * FROM dev.Student;
 
-UPDATE dev.Staff SET phone_number=222 WHERE staff_id = 'LECTURER1';
+
+UPDATE dev.Staff SET phone_number=10101010 WHERE staff_id = 'LECTURER1';
+
+UPDATE dev.Staff SET phone_number=20202020 WHERE staff_id = 'LECTURER2';
+
 UPDATE dev.Staff SET salary=100000000 WHERE staff_id = 'LECTURER1';
+
 UPDATE dev.Staff SET bank_account='1updated' WHERE staff_id = 'LECTURER1';
-SELECT * FROM dev.Staff;

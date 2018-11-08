d5f7fe52686e7292097eeb936a07d6bf74d01d2b
Hady Willi <hadyjanuar95@gmail.com>
2018-11-08 18:49:33 +0800

    Remove all tables but school, staff, student, and university resource, NO TURNING BACK

--- assignment2/1.create-tables.sql
+++ assignment2/1.create-tables.sql
@@ -4,14 +4,14 @@ CONNECT dev/dev@lbactest;
 
 -- Table Creation
 
-DROP    TABLE Bill;
-DROP    TABLE Enrollment;
-DROP    TABLE Course;
-DROP    TABLE Student;
-DROP    TABLE Payroll;
-DROP    TABLE Staff;
-DROP    TABLE School;
-DROP	TABLE UniversityResource;
+DROP    TABLE Bill                  CASCADE CONSTRAINTS;
+DROP    TABLE Enrollment            CASCADE CONSTRAINTS;
+DROP    TABLE Course                CASCADE CONSTRAINTS;
+DROP    TABLE Student               CASCADE CONSTRAINTS;
+DROP    TABLE Payroll               CASCADE CONSTRAINTS;
+DROP    TABLE Staff                 CASCADE CONSTRAINTS;
+DROP    TABLE School                CASCADE CONSTRAINTS;
+DROP	TABLE UniversityResource    CASCADE CONSTRAINTS;
 
 CREATE  TABLE School (
     school_id               int             NOT NULL PRIMARY KEY,
@@ -33,16 +33,6 @@ CREATE  TABLE Staff (
                             CONSTRAINT fkey_staff_school REFERENCES School(school_id)
 );
 
-CREATE  TABLE Payroll (
-    payroll_id              int             NOT NULL PRIMARY KEY,
-    amount                  number(8,2)     NOT NULL,
-    payroll_date            date            NOT NULL,
-    payroll_type            varchar(50)     NOT NULL,
-    payroll_description     varchar(200)    NOT NULL,
-    staff_id                varchar(100)    NOT NULL 
-                            CONSTRAINT fkey_payroll_staff REFERENCES Staff(staff_id)
-);
-
 CREATE  TABLE Student (
     student_id              varchar(100)    NOT NULL PRIMARY KEY,
     name                    varchar(100)    NOT NULL,
@@ -53,40 +43,6 @@ CREATE  TABLE Student (
     school_id               int             NOT NULL   
                             CONSTRAINT fkey_student_school REFERENCES School(school_id)
 );
-
-CREATE  TABLE Course (
-    course_id               varchar(10)     NOT NULL PRIMARY KEY,
-    course_name             varchar(100)    NOT NULL,
-    location                varchar(100)    NOT NULL,
-    class_time              varchar(100)    NOT NULL,
-    semester                int             NOT NULL,
-    year                    int             NOT NULL,
-    school_id               int             NOT NULL   
-                            CONSTRAINT fkey_course_school REFERENCES School(school_id),
-    staff_id                varchar(100)    NOT NULL   
-                            CONSTRAINT fkey_course_staff REFERENCES Staff(staff_id)
-);
-
-CREATE  TABLE Enrollment (
-    student_id              varchar(100)    NOT NULL
-                            CONSTRAINT fkey_enrollment_student REFERENCES Student(student_id),
-    course_id               varchar(10)     NOT NULL
-                            CONSTRAINT fkey_enrollment_course REFERENCES Course(course_id),
-    grade                   NUMBER(6,2)     NOT NULL,
-    CONSTRAINT pkey_enrollment PRIMARY KEY (student_id, course_id)
-);
-
-CREATE  TABLE Bill (
-    bill_id                 int             NOT NULL PRIMARY KEY,
-    semester                int             NOT NULL,
-    fee_type                varchar(20)     NOT NULL,
-    amount                  number(6,2)     NOT NULL,
-    payment_method          varchar(20)     NOT NULL,
-    payment_date            timestamp       NOT NULL,
-    student_id              varchar(100)    NOT NULL   
-                            CONSTRAINT fkey_bill_student REFERENCES Student(student_id)
-);
-
 
 CREATE  TABLE UniversityResource (
    resource_id              int             NOT NULL PRIMARY KEY,
